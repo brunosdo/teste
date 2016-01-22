004d49697698c8f37298cdb175cd4ca1c86d4f1a
Bruno Dumont <bruno.dumont@inss.gov.br>
2016-01-22 16:40:36 -0200

    Primeiro commit
    Change-Id: I1147d4e162dd6728b666d58b64b5ce6349d2e33e

---
+++ TesteGIT/src/br/com/teste/TesteMain.java
@@ -0,0 +1,13 @@
+package br.com.teste;
+
+public class TesteMain {
+
+	public static void main(String[] args) {
+		hello();
+
+	}
+	
+	public static void hello(){
+		System.out.println("Hello");
+	}
+}
